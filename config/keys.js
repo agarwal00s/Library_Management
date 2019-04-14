7c1a543e5a09eeeb8f6ddefbf19c1fffec578270
Sourav Agarwal <agarwalsourav005@gmail.com>
2019-04-15 00:41:44 +0530

    MongoDb Connection and express Router

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://sourav:sourav123@ds145415.mlab.com:45415/sourav_database",
+  localDBURI: "mongodb://localhost:27017/mydb"
+};
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const users = require("./routes/users.js");
+//For mLab
+const mongoose = require("mongoose");
+
+//For Local MongoDb
+const mongoClient = require("mongodb").MongoClient;
+
+//Connecting mLab DB
+const db1 = require("./config/keys.js").mongoURI;
+mongoose
+  .connect(db1, { useNewUrlParser: true })
+  .then(console.log("Database mLab Connected"))
+  .catch(err => console.log(err));
+
+//Connecting Local DB
+const db2 = require("./config/keys.js").localDBURI;
+mongoClient
+  .connect(db2, { useNewUrlParser: true })
+  .then(console.log("Database local Connected"))
+  .catch(err => console.log(err));
+
+//Routing
 const app = express();
 app.get("/", (req, res) => {
-  console.log("Index Page");
   res.send("Hello Sourav");
 });
+app.use("/users", users);
+
+//Listening to port 5000
 app.listen(5000, () => {
   console.log("Server Running");
 });
