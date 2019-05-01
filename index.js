85d69f11900051435f07b174cdf1880bd974ebc1
Sourav Agarwal <agarwalsourav005@gmail.com>
2019-05-01 20:48:18 +0530

    changes

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use("/admin", admins);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.js"));
   });
 }
 
