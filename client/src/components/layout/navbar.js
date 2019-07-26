3d5d42c95b276edaad06b490c319efcad1b3909b
Sourav Agarwal <agarwalsourav005@gmail.com>
2019-07-26 17:26:40 +0530

    Proper Options in Navbar

--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -1,85 +1,94 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
   render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">
-          LIBRARY
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                to="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Student
-              </Link>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <Link className="dropdown-item" to="studentLogin">
-                  SignIn
+    if (this.props.user != null) {
+      return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link className="navbar-brand" to="/">
+            LIBRARY
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/searchBook">
+                  Search Book
                 </Link>
-                <Link className="dropdown-item" to="studentRegister">
-                  Register
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/addBook">
+                  Add Book
                 </Link>
-              </div>
-            </li>
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                to="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Admin
-              </Link>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <Link className="dropdown-item" to="adminLogin">
-                  SignIn
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/issueBook">
+                  Issue Book
                 </Link>
-              </div>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/searchBook">
-                Search Book
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/addBook">
-                Add Book
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/issueBook">
-                Issue Book
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
+    } else {
+      return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link className="navbar-brand" to="/">
+            LIBRARY
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/studentLogin">
+                  Sign In
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/studentRegister">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
+    }
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  user: PropTypes.string
+};
+const mapStateToProps = state => {
+  if (state.auth && state.auth.user) return { user: state.auth.user.name };
+};
+export default connect(mapStateToProps)(Navbar);
