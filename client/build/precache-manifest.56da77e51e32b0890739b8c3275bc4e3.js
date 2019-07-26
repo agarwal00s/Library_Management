cf9560c5335443c07f86227c1243aa9d93c96a5d
Sourav Agarwal <agarwalsourav005@gmail.com>
2019-07-26 20:47:54 +0530

    Fix

---
+++ client/build/precache-manifest.56da77e51e32b0890739b8c3275bc4e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a1533c905a583ee27ee",
+    "url": "/static/css/main.0779e4bd.chunk.css"
+  },
+  {
+    "revision": "6a1533c905a583ee27ee",
+    "url": "/static/js/main.2566878f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6127a266c9adf04b991b",
+    "url": "/static/js/2.4d9b07d9.chunk.js"
+  },
+  {
+    "revision": "67a4bc59544213f58409c2ae9e70d548",
+    "url": "/index.html"
+  }
+];
